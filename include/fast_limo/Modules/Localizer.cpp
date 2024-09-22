842eb3765d8f3a5d8b7da4cccca9fb10f913bca7
fetty <fetty3113@gmail.com>
2024-09-22 17:16:41 +0200

    New binary search added

--- include/fast_limo/Modules/Localizer.cpp
+++ include/fast_limo/Modules/Localizer.cpp
@@ -787,38 +787,33 @@
 
             this->last_state = fast_limo::State(this->_iKFoM.get_x()); // baselink/body frame
 
-            int k = 0;
-            for (int i = 0; i < frames.size()-1; i++) {
+            #pragma omp parallel for num_threads(this->num_threads_)
+            for (int k = 0; k < deskewed_scan_->points.size(); k++) {
 
-                while( k < deskewed_scan_->points.size() &&
-                        frames[i].time <= extract_point_time(deskewed_scan_->points[k])+offset && 
-                        extract_point_time(deskewed_scan_->points[k])+offset <= frames[i+1].time
-                    ){
+                int i_f = algorithms::binary_search_tailored(frames, extract_point_time(deskewed_scan_->points[k])+offset);
 
-                    State X0 = frames[i];
-                    X0.update(extract_point_time(deskewed_scan_->points[k]) + offset);
+                State X0 = frames[i_f];
+                X0.update(extract_point_time(deskewed_scan_->points[k]) + offset);
 
-                    Eigen::Matrix4f T = X0.get_RT() * this->extr.lidar2baselink_T;
+                Eigen::Matrix4f T = X0.get_RT() * this->extr.lidar2baselink_T;
 
-                    // world frame deskewed pc
-                    auto &pt = deskewed_scan_->points[k]; // lidar frame
-                    pt.getVector4fMap()[3] = 1.;
-                    pt.getVector4fMap() = T * pt.getVector4fMap(); // world/global frame
+                // world frame deskewed pc
+                auto &pt = deskewed_scan_->points[k]; // lidar frame
+                pt.getVector4fMap()[3] = 1.;
+                pt.getVector4fMap() = T * pt.getVector4fMap(); // world/global frame
 
-                    // Xt2 frame deskewed pc
-                    auto pt2 = deskewed_scan_->points[k];
-                    pt2.getVector4fMap() = this->last_state.get_RT_inv() * pt.getVector4fMap(); // Xt2 frame
-                    pt2.intensity = pt.intensity;
+                // Xt2 frame deskewed pc
+                auto pt2 = deskewed_scan_->points[k];
+                pt2.getVector4fMap() = this->last_state.get_RT_inv() * pt.getVector4fMap(); // Xt2 frame
+                pt2.intensity = pt.intensity;
 
-                    if(this->isInRange(pt2)) 
-                        deskewed_Xt2_scan_->points.push_back(pt2);
+                if(this->isInRange(pt2)) 
+                    deskewed_Xt2_scan_->points.push_back(pt2);
 
-                    ++k;
-                }
             }
 
             // debug info
-            this->deskew_size = k; 
+            this->deskew_size = deskewed_Xt2_scan_->points.size(); 
             this->propagated_size = frames.size();
 
             if(this->config.debug && this->deskew_size > 0) // debug only
